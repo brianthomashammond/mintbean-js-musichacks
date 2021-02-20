2057a807e00059abe95eaffb7a11b54e43b75b76
brianthomashammond <brianthomashammond@gmail.com>
2021-02-20 11:18:27 -0800

    adds key color change on mouseclick

--- src/App.js
+++ src/App.js
@@ -23,19 +23,19 @@ function App() {
         <div classname="pianoPage">
             <h1>Piano</h1>
             <div className="piano">
-                <div className="white-key" onClick={playC4}>A</div>
-                <div className="black-key" onClick={playDb4}>W</div>
-                <div className="white-key" onClick={playD4}>S</div>
-                <div className="black-key" onClick={playEb4}>E</div>
-                <div className="white-key" onClick={playE4}>D</div>
-                <div className="white-key" onClick={playF4}>F</div>
-                <div className="black-key" onClick={playGb4}>T</div>
-                <div className="white-key" onClick={playG4}>G</div>
-                <div className="black-key" onClick={playAb4}>Y</div>
-                <div className="white-key" onClick={playA4}>H</div>
-                <div className="black-key" onClick={playBb4}>U</div>
-                <div className="white-key" onClick={playB4}>J</div>
-                <div className="white-key" onClick={playC5}>K</div>
+                <div className="white-key C" onClick={playC4}>A</div>
+                <div className="black-key Db" onClick={playDb4}>W</div>
+                <div className="white-key D" onClick={playD4}>S</div>
+                <div className="black-key Eb" onClick={playEb4}>E</div>
+                <div className="white-key E" onClick={playE4}>D</div>
+                <div className="white-key F" onClick={playF4}>F</div>
+                <div className="black-key Gb" onClick={playGb4}>T</div>
+                <div className="white-key G" onClick={playG4}>G</div>
+                <div className="black-key Ab" onClick={playAb4}>Y</div>
+                <div className="white-key A" onClick={playA4}>H</div>
+                <div className="black-key Bb" onClick={playBb4}>U</div>
+                <div className="white-key B" onClick={playB4}>J</div>
+                <div className="white-key C" onClick={playC5}>K</div>
             </div>
         </div>
     )
