eeb6ccb1651dbb5e87cef18c7ec834387c00d8b1
brianthomashammond <brianthomashammond@gmail.com>
2021-02-19 21:28:33 -0800

    Initial commit, single tone achieved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import * as Tone from 'tone'
+
+function playC4() {
+    const synth = new Tone.Synth().toDestination()
+    synth.triggerAttackRelease("C4", "8n")
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <button onClick={playC4}>Play Me</button>
     </div>
   );
 }
