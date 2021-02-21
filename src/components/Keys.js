9c457ddad9bfffc02209808308b4e8903ea3d3d5
AKFosterDev <akfoster2015@gmail.com>
2021-02-21 15:26:49 -0800

    complete component functionality

--- src/components/Keys.js
+++ src/components/Keys.js
@@ -1,15 +1,36 @@
 import React, { Fragment } from 'react'
 import Key from './Key'
 import notes from '../notes'
+import {
+	playC4,
+	playDb4,
+	playD4,
+	playEb4,
+	playE4,
+	playF4,
+	playGb4,
+	playG4,
+	playAb4,
+	playA4,
+	playBb4,
+	playB4,
+	playC5,
+	playNote,
+	playTwinkleTwinkle,
+	playPolyphonicExample,
+} from '../tone.fn.js'
 
 const Keys = () => {
+	// handleNote = tone => {}
+
 	return (
 		<Fragment>
 			{notes.map(note => (
 				<Key
-					className={note.className}
-					onClick={note.onClick}
-					value={note.value}
+					note={note.note}
+					color={note.color}
+					qwerty={note.qwerty}
+					tone={note.tone}
 				/>
 			))}
 		</Fragment>
