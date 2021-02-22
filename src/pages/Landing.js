40ba38516d2eed483f51d690b121a12e68881524
AKFosterDev <akfoster2015@gmail.com>
2021-02-22 07:09:25 -0800

    add demo button

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import '../App.css'
 import music from '../assets/music.png'
 import Team from '../components/Team'
@@ -32,6 +33,9 @@ const Landing = () => {
 						<Summary />
 					</div>
 				</div>
+				<Link to='/demo'>
+					<button className='demo'>Demo</button>
+				</Link>
 			</div>
 		</Fragment>
 	)
