89354d02458959dc12fbc3ece2cd48a6f5bd16d7
brianthomashammond <brianthomashammond@gmail.com>
2021-02-22 20:34:00 -0800

    many octaves, narrow keys

--- src/notes.js
+++ src/notes.js
@@ -1,4 +1,32 @@
 const notes = [
+    {
+		color: 'white',
+		tone: 'F3',
+	},
+    {
+		color: 'black',
+		tone: 'Gb3',
+	},
+    {
+		color: 'white',
+		tone: 'G3',
+	},
+    {
+		color: 'black',
+		tone: 'Ab3',
+	},
+    {
+		color: 'white',
+		tone: 'A3',
+	},
+    {
+		color: 'black',
+		tone: 'Bb3',
+	},
+    {
+		color: 'white',
+		tone: 'B3',
+	},
 	{
 		color: 'white',
 		tone: 'C4',
@@ -50,6 +78,70 @@ const notes = [
 	{
 		color: 'white',
 		tone: 'C5',
+	},
+    {
+		color: 'black',
+		tone: 'Db5',
+	},
+	{
+		color: 'white',
+		tone: 'D5',
+	},
+	{
+		color: 'black',
+		tone: 'Eb5',
+	},
+	{
+		color: 'white',
+		tone: 'E5',
+	},
+	{
+		color: 'white',
+		tone: 'F5',
+	},
+	{
+		color: 'black',
+		tone: 'Gb5',
+	},
+	{
+		color: 'white',
+		tone: 'G5',
+	},
+	{
+		color: 'black',
+		tone: 'Ab5',
+	},
+	{
+		color: 'white',
+		tone: 'A5',
+	},
+	{
+		color: 'black',
+		tone: 'Bb5',
+	},
+	{
+		color: 'white',
+		tone: 'B5',
+	},
+	{
+		color: 'white',
+		tone: 'C6',
+    },
+    {
+		color: 'black',
+		tone: 'Db6',
+	},
+	{
+		color: 'white',
+		tone: 'D6',
+	},
+	{
+		color: 'black',
+		tone: 'Eb6',
+	},
+	{
+		color: 'white',
+		tone: 'E6',
 	},
 ]
 
