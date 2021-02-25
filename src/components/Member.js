b25c3ff70b4c37632a3c98f967996dd4670291be
mabrouk10 <samimabrouk10@gmail.com>
2021-02-25 08:14:31 -0800

    added github and linkedin icons

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 
-const Member = ({ img, name }) => {
+const Member = ({ img, name, linkedin, github }) => {
 	return (
 		<div>
 			<img src={img} alt={name} />
 			<p className='team-member-name'>{name}</p>
+			<Link className="icon" target="_blank" to={github}> <FaGithub /> </Link>
+			<Link className="icon" target="_blank" to={linkedin}> <FaLinkedin /> </Link>
 		</div>
 	)
 }
